eaa25a5cb2cff9e8e7e38087bf808f142d64b65c
Rahul Kumar <rvideep4@gmail.com>
2023-03-02 15:44:20 +0530

    test1 add todo lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Todo from './usereducer/Todo';
+import "./App.css";
+import Todo from "./usereducer/Todo";
 // import Render from './useref/Render';
 
 function App() {
   return (
     <div className="App">
       {/* <Render /> */}
+      <h1>Todo Lists</h1>
       <Todo />
     </div>
   );
